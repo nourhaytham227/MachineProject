f01aa1823bde03e91173f63325ea4607914b0407
nourhaytham227 <161311505+nourhaytham227@users.noreply.github.com>
2025-11-27 18:19:51 +0000

    modified:   LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -9,12 +9,40 @@ from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 
 df= pd.read_csv("insurance.csv")
-print(df.head())
-print(df.info())
+#print(df.head())
+#print(df.info())
 
 target='charges'
 features=['age', 'sex', 'bmi', 'children', 'smoker', 'region']
 x= df[features].values
 y= df[target]
-print(x)
-print(y)
+
+#preprocessing 
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1, 4, 5])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+
+#Splitting the dataset
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+#Feature Scaling
+scaler= StandardScaler()
+x_train= scaler.fit_transform(x_train)
+x_test= scaler.transform(x_test)
+
+#Training model
+model= LinearRegression()
+model.fit(x_train, y_train)
+
+#Predicting test set results
+y_pred= model.predict(x_test)
+mse= mean_squared_error(y_test, y_pred)
+print("Mean Squared Error:", mse)
+
+#Visualizing Actual vs Predicted
+plt.scatter(y_test, y_pred, color='blue')
+plt.xlabel("Actual Charges")
+plt.ylabel("Predicted Charges")
+plt.title("Actual vs Predicted Medical Charges")
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'k--', lw=2)
+plt.show()
+
