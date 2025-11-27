46f4f2cb9f5b9d91551e16a2afaab3a7cb91cbc4
nourhaytham227 <161311505+nourhaytham227@users.noreply.github.com>
2025-11-27 18:03:46 +0000

    modified:   LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,1 +1,20 @@
 import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+
+df= pd.read_csv("insurance.csv")
+print(df.head())
+print(df.info())
+
+target='charges'
+features=['age', 'sex', 'bmi', 'children', 'smoker', 'region']
+x= df[features].values
+y= df[target]
+print(x)
+print(y)
